d9d280f0cda235feea48135d1213887a03e0e2d3
Amit Hassan Joy <sarkeramit88@gmail.com>
2023-08-06 13:17:09 +0600

    Game PAUSE added
    declearing game pause and play state to identify the game state and by pressing space we can start new game if the game is over. To pause game press P .

--- src/Shape.java
+++ src/Shape.java
@@ -141,7 +141,23 @@ public class Shape {
     {
         int [] [] rotateShape = transposeMatrix(coordis) ;
         reverse(rotateShape);
-
+        //Check for right side and bottom
+        if( (x+rotateShape[0].length >Board.BOARD_WIDTH) || (y+rotateShape.length > 20))
+        {
+            return;
+        }
+        //Check for Collision with other shapes before rotated
+        for(int row=0;row< rotateShape.length ;row++)
+        {
+            for(int col =0;col< rotateShape[row].length ; col++)
+            {
+                if(rotateShape[row][col] !=0)
+                {
+                    if(board.getBoard()[y+ row ][x+col] != null)
+                        return;
+                }
+            }
+        }
         coordis = rotateShape ;
     }
 
@@ -185,6 +201,12 @@ public class Shape {
             }
         }
     }
+
+    public int[][] getCoordis()
+    {
+        return coordis ;
+    }
+
     public void speedUp()
     {
         delayTimeForMovement = fast ;
@@ -200,6 +222,16 @@ public class Shape {
     public void moveLeft()
     {
         deltaX = -1 ;
+    }
+
+    public  int getY()
+    {
+        return y;
+    }
+
+    public  int getX()
+    {
+        return x;
     }
 
 }
